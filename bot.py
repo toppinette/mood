692de2461360e2c80125812a86a5f3a6ea88cc97
toppinette <toppinette@gmail.com>
2024-10-20 18:59:06 +0200

    Fix web process

--- bot.py
+++ bot.py
@@ -160,5 +160,7 @@ def main():
 if __name__ == '__main__':
     main()
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
