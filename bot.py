3b7473a07702fef99bcab53a3c2faba23caae2f6
toppinette <toppinette@gmail.com>
2025-05-25 20:29:25 +0200

    fix: split config files from bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def build_currency_keyboard():
     return InlineKeyboardMarkup([
         [InlineKeyboardButton("Euro", callback_data="Euro"), InlineKeyboardButton("RSD", callback_data="RSD")],
         [InlineKeyboardButton("pounds", callback_data="pounds"), InlineKeyboardButton("руб", callback_data="руб")],
-        [InlineKeyboardButton("usd", callback_data="usd"), InlineKeyboardButton("tenge", callback_data="tenge")],
+        [InlineKeyboardButton("USD", callback_data="USD"), InlineKeyboardButton("tenge", callback_data="tenge")],
     ])
 
 def build_category_keyboard():
